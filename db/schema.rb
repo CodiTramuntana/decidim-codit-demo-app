901becb2c50c20aa1873b908f08d5c508d60d29a
Laura Jaime <laura.jv@coditramuntana.com>
2024-07-15 16:38:22 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_20_090860) do
+ActiveRecord::Schema.define(version: 2024_07_15_142358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
