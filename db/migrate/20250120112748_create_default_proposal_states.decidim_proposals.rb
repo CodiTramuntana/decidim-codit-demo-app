6fdccfaa73282740a37d97aaa2d11536bccf40c2
Laura Jaime <laura.jv@coditramuntana.com>
2026-02-18 12:52:08 +0100

    Changes in migrations because an error with deleted_at field

--- db/migrate/20250120112748_create_default_proposal_states.decidim_proposals.rb
+++ db/migrate/20250120112748_create_default_proposal_states.decidim_proposals.rb
@@ -16,19 +16,19 @@ class CreateDefaultProposalStates < ActiveRecord::Migration[6.1]
   end
 
   def up
-    CustomProposal.reset_column_information
-    Decidim::Proposals::ProposalState.reset_column_information
-    Decidim::Component.unscoped.where(manifest_name: "proposals").find_each do |component|
-      admin_user = component.organization.admins.first
-      default_states = Decidim::Proposals.create_default_states!(component, admin_user)
+    # CustomProposal.reset_column_information
+    # Decidim::Proposals::ProposalState.reset_column_information
+    # Decidim::Component.unscoped.where(manifest_name: "proposals").find_each do |component|
+    #   admin_user = component.organization.admins.first
+    #   default_states = Decidim::Proposals.create_default_states!(component, admin_user)
 
-      CustomProposal.where(decidim_component_id: component.id).find_each do |proposal|
-        next if proposal.old_state == "not_answered"
+    #   CustomProposal.where(decidim_component_id: component.id).find_each do |proposal|
+    #     next if proposal.old_state == "not_answered"
 
-        token = default_states[proposal.old_state.to_sym][:object]&.token
-        proposal.update!(proposal_state: Decidim::Proposals::ProposalState.where(component:, token:).first!)
-      end
-    end
+    #     token = default_states[proposal.old_state.to_sym][:object]&.token
+    #     proposal.update!(proposal_state: Decidim::Proposals::ProposalState.where(component:, token:).first!)
+    #   end
+    # end
   end
 
   def down
